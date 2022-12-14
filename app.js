f3c146b5b301ab368dd2d546545fd8760c54ba24
OFFICE\jupit <som.katarey@jupitice.com>
2022-12-14 14:50:52 +0530

    changes

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ const express = require('express');
 const mysql = require("mysql")
 const dotenv = require('dotenv')
 const app = express();
+const hbs = require('hbs');
 const path = require("path");
+//const publicDir = path.join(__dirname, './public')
 
 app.use(express.json())
 app.set('view engine', 'hbs')
+app.set("views", path.join(__dirname,"/public"));
 //app.use(express.static(publicDir))
 
-app.use(express.urlencoded({extended: 'false'}))
-// app.use(express.json())
+//app.use(express.urlencoded({extended: 'false'}))
+ app.use(express.json())
+ app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
 //middleware to read req.body.<params>
 //CREATE USER
-//const publicDir = path.join(__dirname, './public')
+
 
 dotenv.config({ path: './.env'})
 
@@ -34,6 +39,29 @@ db.connect((error) => {
         console.log("MySQL connected!")
     }
 })
+
+
+
+app.get("/", (req, res) => {
+    res.render("index", {
+    name: "Kushan", age: 24
+});});
+
+
+app.get("/login", (req, res) => {
+    res.render("login", {
+    name: "Kushan", age: 24
+});});
+
+
+app.get("/register", (req, res) => {
+    res.render("login", {
+    name: "Kushan", age: 24
+});});
+
+
+
+
 
 app.get("/users", (req, res) => {
    var data;
@@ -60,8 +88,8 @@ app.get("/users", (req, res) => {
     const email=req.body.email;
     const psw=req.body.psw;
     console.log(uname);
-    db.query('SELECT * FROM users WHERE uname=?',uname, function(err, rows, fields) 
-    {
+     db.query('SELECT * FROM users WHERE uname=?',uname, function(err, rows, fields) 
+     {
       if (err) throw err;
        data=rows;
       console.log(rows[0]);
@@ -73,27 +101,64 @@ app.get("/users", (req, res) => {
       }
      else{
         db.query(' INSERT INTO users VALUES(?,?,?)',[uname,email,psw], function(err, rows, fields) 
-        {
+         {
             if (err) throw err;
             else
             {
                 console.log("Inserted Successfully");
             }
-            return res.render('login', {
-                message: 'registered Succesfully'
+           return res.render('login', {
+                 message: 'registered Succesfully'
+             })
             })
+    }
 
         })
-     }
-      
-    
-    //   res.json({data});
-    
-    });
+    })
 
+
+
+        app.post("/login", (req, res) => {
+
+            const uname = req.body.uname;
+            const email=req.body.email;
+            const psw=req.body.psw;
+            console.log(uname);
+            var query=db.query('SELECT * FROM users WHERE uname=? AND psw=?',[uname,psw], function(err, rows, fields) 
+            {
+              if (err) throw err;
+               data=rows;
+              console.log(rows[0]);
+              console.log(query.sql)
+              if(data.length>0)
+              {
+                return res.render('welcome', {
+                                message: 'Welcome'
+                            })
+              }
+             else{
+                console.log("incrorrect");
+               
+                    return res.render('registration', {
+                        message: 'please Register'
+                    })
+          
+                
+             }
+              
+        
+        //   res.json({data});
+        
+        });
     
-            
-});
+        
+                
+    });
+    
+    
+     
+
+
 
 
 
@@ -102,3 +167,8 @@ app.get("/users", (req, res) => {
 app.listen(5000, ()=> {
         console.log("server started on port 5000")
     })
+
+
+
+
+   
